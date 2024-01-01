a1e20a6698bc7e89d74ae9c48494453d4696b97e
wladimirgrf <wladimir.code@gmail.com>
2024-01-01 17:26:31 -0300

    Componente: Heading

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  tittle: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom Title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,4 @@
 export * from './components/Box'
 export * from './components/Button'
 export * from './components/Text'
+export * from './components/Heading'
